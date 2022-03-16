5f483f56a6b0c855c143b24e30d8ef105a46c295
陳泰元 <chentaiyuan@chentaiyuans-Mac-mini.local>
2022-03-16 17:41:47 +0800

    [Add]add dataset loading and main

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from config.constant import FILE_PATH
+from dataprocess.dataset import DataSet
+
+toggle_option = input("Enter toggle option(0: discrete/1:continuous):")
+
+dataset = DataSet(FILE_PATH)
+
+# get training/testing data (pics, labels)
+train_x, train_y = dataset.get_training_data()
+test_x, test_y = dataset.get_testing_data()
+
+if toggle_option == "0":
+    # discrete
+    # TODO: Get probability matrix
+    # TODO: Get prior
+    # TODO: Get and print posterior
+    # TODO: print the pic based on likelihood
+    pass
+else:
+    # continuous
+    # TODO: Get probability matrix
+    # TODO: Get prior
+    # TODO: Get and print posterior
+    # TODO: print the pic based on likelihood
+    pass
+
