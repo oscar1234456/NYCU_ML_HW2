94f94dd89553c70ff9d81cc370d6a73b45ca619d
陳泰元 <chentaiyuan@chentaiyuans-Mac-mini.local>
2022-03-16 17:49:07 +0800

    [Add]aff prob matrix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from config.constant import FILE_PATH
 from dataprocess.dataset import DataSet
+from process.get_proba_matrix import get_prob_matrix_discrete, get_prob_matrix_continuous
 
 toggle_option = input("Enter toggle option(0: discrete/1:continuous):")
 
@@ -14,6 +15,7 @@ test_x, test_y = dataset.get_testing_data()
 if toggle_option == "0":
     # discrete
     # TODO: Get probability matrix
+    prob_matrix_discrete = get_prob_matrix_discrete(train_x, train_y)
     # TODO: Get prior
     # TODO: Get and print posterior
     # TODO: print the pic based on likelihood
@@ -21,6 +23,7 @@ if toggle_option == "0":
 else:
     # continuous
     # TODO: Get probability matrix
+    prob_matrix_continuous = get_prob_matrix_continuous(train_x, train_y)
     # TODO: Get prior
     # TODO: Get and print posterior
     # TODO: print the pic based on likelihood
---
+++ process/get_proba_matrix.py
@@ -0,0 +1,9 @@
+def get_prob_matrix_discrete(train_x, train_y):
+    pass
+
+
+
+
+
+def get_prob_matrix_continuous(train_x, train_y):
+    pass
